d8cc4e167d22130528012f68665dac0eb00d427d
konsan1101 <konchan@gold.ocn.ne.jp>
2019-06-15 07:41:28 +0900

    backup

--- _v5__main__compile.bat
+++ _v5__main__compile.bat
@@ -1,12 +1,12 @@
 @echo off
 cd "C:\Users\kondou\Documents\GitHub\pycv5"
 
-pyinstaller _v5__main_video.py --onefile
+pyinstaller _v5__main_camera.py --onefile
 
 pause
 
-del  "_v5__main_video.spec"
-copy "C:\Users\kondou\Documents\GitHub\pycv5\dist\_v5__main_video.exe"  "_v5__main_video.exe"
+del  "_v5__main_camera.spec"
+copy "C:\Users\kondou\Documents\GitHub\pycv5\dist\_v5__main_camera.exe"  "_v5__main_camera.exe"
 
 pause
 
