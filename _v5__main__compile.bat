a2148216efbd1b3fda1a50324a5505240de04ed8
konsan1101 <konchan@gold.ocn.ne.jp>
2019-06-17 07:49:35 +0900

    Update _v5__main__compile.bat

--- _v5__main__compile.bat
+++ _v5__main__compile.bat
@@ -1,15 +1,20 @@
 @echo off
 cd "C:\Users\kondou\Documents\GitHub\pycv5"
 
-pyinstaller _v5__main_camera.py --onefile
-pyinstaller _v5__main_audio.py  --onefile
+    pyinstaller _v5__main_camera.py --onefile
+    pyinstaller _v5__main_audio.py  --onefile
+rem pyinstaller _v5__main_video.py  --onefile
 
 pause
 
-del  "_v5__main_camera.spec"
-del  "_v5__main_audio.spec"
-copy "C:\Users\kondou\Documents\GitHub\pycv5\dist\_v5__main_camera.exe"  "_v5__main_camera.exe"
-copy "C:\Users\kondou\Documents\GitHub\pycv5\dist\_v5__main_audio.exe"   "_v5__main_audio.exe"
+    copy "C:\Users\kondou\Documents\GitHub\pycv5\dist\_v5__main_camera.exe"  "_v5__main_camera.exe"
+    del  "_v5__main_camera.spec"
+
+    copy "C:\Users\kondou\Documents\GitHub\pycv5\dist\_v5__main_audio.exe"   "_v5__main_audio.exe"
+    del  "_v5__main_audio.spec"
+
+rem copy "C:\Users\kondou\Documents\GitHub\pycv5\dist\_v5__main_video.exe"   "_v5__main_video.exe"
+rem del  "_v5__main_video.spec"
 
 pause
 
