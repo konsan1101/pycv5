ed8f214f27327cc2df8460cc8445acdc44e1104c
konsan1101 <konchan@gold.ocn.ne.jp>
2019-06-23 21:15:08 +0900

    backup

---
+++ speech_api_hoya_key_sample.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+
+def getkey():
+
+    # hoya 音声合成
+    print('speech_api_hoya_key.py')
+    print('set your key!')
+    return 'your key'
+
+
---
+++ speech_api_weather_key_sample.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+
+def getkey(api,):
+
+    # openweathermap 天気予報
+    if (api == 'openweathermap'):
+        print('speech_api_weather_key.py')
+        print('set your key!')
+        return 'your key'
+
+    return False
+
+
