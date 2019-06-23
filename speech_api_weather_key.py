f3eff868529ce0b5261733970db3002d8a675e2d
konsan1101 <konchan@gold.ocn.ne.jp>
2019-06-23 21:04:56 +0900

    backup

--- speech_api_weather_key.py
+++ speech_api_weather_key.py
@@ -9,7 +9,7 @@ def getkey(api,):
     if (api == 'openweathermap'):
         print('speech_api_weather_key.py')
         print('set your key!')
-        return 'your key'
+        return 'your key '
 
     return False
 
