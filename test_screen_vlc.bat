b9bf1f813942c301cc2dc38862afcf5c68bed9f2
Mitsuo KONDOU <konchan@gold.ocn.ne.jp>
2019-07-17 17:21:55 +0900

    Update test_screen_vlc.bat

--- test_screen_vlc.bat
+++ test_screen_vlc.bat
@@ -1,6 +1,10 @@
-start "rec" vlc screen:// :screen-fps=5 --sout=#transcode{vcodec=h264,acodec=none}:standard{access=file,mux=mp4,dst=TEST.FLV}
+@echo off
 
-ping localhost -w 1000 -n 10 >nul
+rem start "rec" vlc screen:// :screen-fps=5 --sout=#transcode{vcodec=h264,acodec=none}:standard{access=file,mux=mp4,dst=test.mp4}
+rem start "rec" vlc screen:// :screen-fps=5 --sout=#transcode{vcodec=h264,acodec=none}:standard{access=file,mux=mkv,dst=test.mkv}
+    start "rec" vlc screen:// :screen-fps=5 --sout=#transcode{vcodec=h264,acodec=none}:standard{access=file,mux=flv,dst=test.flv}
+
+ping localhost -w 1000 -n 20 >nul
 
 TASKKILL /IM VLC.EXE
 
