f9604a1cc39c5345e3538c227f9c76c4f5e9735f
Mitsuo KONDOU <konchan@gold.ocn.ne.jp>
2019-07-17 09:11:37 +0900

    beckup

--- _v5_api_vision.py
+++ _v5_api_vision.py
@@ -407,23 +407,23 @@ class api_vision_class:
                 )
 
         # threading
-        if (sync != True):
-            self.vision_proc = threading.Thread(target=vision_batch, args=(
-                runMode, camDev,
-                qApiCV, qApiOCR, qApiTrn, qLangCV, qLangOCR, qLangTrn,
-                procId, fileId, inpCV, tmpCV, outCV, inpOCR, tmpOCR, outOCR, outTrn,
-                ))
-            self.vision_proc.setDaemon(True)
-            self.vision_proc.start()
-
-        # multiprocessing.Process
         #if (sync != True):
-        #    self.vision_proc = multiprocessing.Process(target=vision_batch, args=(
+        #    self.vision_proc = threading.Thread(target=vision_batch, args=(
         #        runMode, camDev,
         #        qApiCV, qApiOCR, qApiTrn, qLangCV, qLangOCR, qLangTrn,
         #        procId, fileId, inpCV, tmpCV, outCV, inpOCR, tmpOCR, outOCR, outTrn,
         #        ))
+        #    self.vision_proc.setDaemon(True)
         #    self.vision_proc.start()
+
+        # multiprocessing.Process
+        if (sync != True):
+            self.vision_proc = multiprocessing.Process(target=vision_batch, args=(
+                runMode, camDev,
+                qApiCV, qApiOCR, qApiTrn, qLangCV, qLangOCR, qLangTrn,
+                procId, fileId, inpCV, tmpCV, outCV, inpOCR, tmpOCR, outOCR, outTrn,
+                ))
+            self.vision_proc.start()
 
         #if (sync == True):
         #    self.vision_proc.join()
