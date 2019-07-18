a06296a4349838eca83b259d76a86d50ee0d5e50
Mitsuo KONDOU <konchan@gold.ocn.ne.jp>
2019-07-18 09:48:14 +0900

    backup

--- test_screen_vlc.bat
+++ test_screen_vlc.bat
@@ -1,11 +1,17 @@
 @echo off
 
-start "rec" vlc screen:// :screen-fps=5 --sout=#transcode{vcodec=h264,acodec=none}:standard{access=file,mux=flv,dst=test.flv} --qt-start-minimized
+set ext=mp4
+
+if exist "desktop.%ext%"  del "desktop.%ext%"
+
+start "" /b vlc screen:// :screen-fps=5 :live-caching=300 --sout=#transcode{vcodec=h264,acodec=none}:standard{access=file,mux=%ext%,dst="desktop.%ext%"} --qt-start-minimized
 
 ping localhost -w 1000 -n 30 >nul
 
-taskkill /im vlc.exe
+vlc vlc://quit
 
-PAUSE
+ping localhost -w 1000 -n 5 >nul
+
+taskkill /im vlc.exe /f
 
 
