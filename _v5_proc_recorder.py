36975955379c6b524016dc7cd161fedac54ddc06
konsan1101 <konchan@gold.ocn.ne.jp>
2019-07-26 07:35:26 +0900

    Update _v5_proc_recorder.py

--- _v5_proc_recorder.py
+++ _v5_proc_recorder.py
@@ -202,7 +202,7 @@ class proc_recorder:
                         rec_ffmpeg.send_signal(signal.SIGINT)
                     else:
                         rec_ffmpeg.send_signal(signal.CTRL_C_EVENT)
-                    time.sleep(5.00)
+                    time.sleep(10.00)
                     rec_ffmpeg.terminate()
                     rec_ffmpeg = None
 
