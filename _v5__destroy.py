8eda00823f5f87c97c02c80ce2c04faa64d43cff
konsan1101 <konchan@gold.ocn.ne.jp>
2019-08-05 20:44:17 +0900

    backup

--- _v5__destroy.py
+++ _v5__destroy.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# COPYRIGHT (C) 2019 Mitsuo KONDOU.
+# This software is released under the MIT License.
+# https://github.com/konsan1101
+# Thank you for keeping the rules.
+
+
+
 # qFunc 共通ルーチン
 import  _v5__qFunc
 qFunc = _v5__qFunc.qFunc_class()
--- speech_api_nict_key_sample.py
+++ speech_api_nict_key_sample.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# COPYRIGHT (C) 2019 Mitsuo KONDOU.
+# This software is released under the MIT License.
+# https://github.com/konsan1101
+# Thank you for keeping the rules.
+
 
 
 def getkey(api, key):
