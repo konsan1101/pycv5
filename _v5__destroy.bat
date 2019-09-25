91660445153ce1e1c0c312614e336dc036eea0c9
konsan1101 <konchan@gold.ocn.ne.jp>
2019-09-26 07:42:13 +0900

    Update _v5__destroy.bat

--- _v5__destroy.bat
+++ _v5__destroy.bat
@@ -1,12 +1,16 @@
 @ECHO OFF
 
-rem ECHO;
-rem ECHO taskkill /im python.exe /f
-rem      taskkill /im python.exe /f
+ECHO;
+ECHO taskkill /im python.exe /f
+     taskkill /im python.exe /f
 
 ECHO;
 ECHO python _v5__destroy.py
      python _v5__destroy.py
+
+ECHO;
+ECHO taskkill /im python.exe /f
+     taskkill /im python.exe /f
 
 ping localhost -w 1000 -n 5 >nul
 
